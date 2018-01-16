1afd4c798161a4270be4f33cbae80b3b5e9dc5c6
Julia Muzafarova <shestakovauv@gmail.com>
2018-01-16 17:50:10 +0300

    Bem-design page

---
+++ common.blocks/cdn/cdn.deps.js
@@ -0,0 +1,208 @@
+([
+	{
+		shouldDeps: [
+			{ block: 'pt-avatar' },
+			{ block: 'pt-avatar', elem: 'photo', elemMods: { size: ['xs', 's', 'm', 'l'] } },
+
+
+
+			{ block: 'pt-badge', mods: { view: ['alert', 'default', 'inverse', 'success', 'warning'] } },
+
+
+
+			{ block: 'pt-card', mods: { border: ['all'], 
+										shadow: ['cloud'], 
+										view: ['default', 'inverse'] } },
+			{ block: 'pt-card', elem: 'content', elemMods: { 'space-a': ['xs', 's', 'm', 'l', 'xl', 'xxl'], 
+															 'space-b': ['xs', 's', 'm', 'l', 'xl', 'xxl'],
+															 'space-h': ['xs', 's', 'm', 'l', 'xl', 'xxl'],
+															 'space-l': ['xs', 's', 'm', 'l', 'xl', 'xxl'],
+															 'space-r': ['xs', 's', 'm', 'l', 'xl', 'xxl'],
+															 'space-t': ['xs', 's', 'm', 'l', 'xl', 'xxl'],
+															 'space-v': ['xs', 's', 'm', 'l', 'xl', 'xxl'] } },
+			{ block: 'pt-card', elem: 'footer', elemMods: { 'align': ['between'], 
+															'space-a': ['xs', 's', 'm', 'l', 'xl', 'xxl'],
+															'space-b': ['xs', 's', 'm', 'l', 'xl', 'xxl'],
+															'space-h': ['xs', 's', 'm', 'l', 'xl', 'xxl'],
+															'space-v': ['xs', 's', 'm', 'l', 'xl', 'xxl'] } },
+			{ block: 'pt-card', elem: 'header', elemMods: { 'space-a': ['xs', 's', 'm', 'l', 'xl', 'xxl'],
+															'space-h': ['xs', 's', 'm', 'l', 'xl', 'xxl'],
+															'space-v': ['xs', 's', 'm', 'l', 'xl', 'xxl'] } },
+			{ block: 'pt-card', elem: 'image', elemMods: { 'shadow': ['bottom-default', 'bottom-inverse', 'top-default', 'top-inverse'],
+														   'size': ['cover'] } },
+			{ block: 'pt-card', elem: 'item' },
+			{ block: 'pt-card', elem: 'play' },
+
+
+
+			{ block: 'pt-dialog', mods: { align: ['action'], 
+										  border: ['all'], 
+										  view: ['default', 'inverse', 'ghost'] } },
+			{ block: 'pt-dialog', elem: 'action' },
+			{ block: 'pt-dialog', elem: 'content' },
+
+
+
+			{ block: 'pt-flex', mods: { border: 'all',
+										shadow: 'cloud',
+									   'space-a': ['xs', 's', 'm', 'l'],
+									   'space-h': ['xs', 's', 'm', 'l'],
+									   'space-v': ['xs', 's', 'm', 'l'],
+									   'status': ['alert', 'default', 'success', 'warning'],
+									   'view': 'stripe' } },
+			{ block: 'pt-flex', elem: 'col', elemMods: { align: ['center', 'right'],
+														 width: ['5', '10', '15', '20', '25', '30', '35', '40', '45', '50', '55', '60', '65', '70', '75', '80', '85', '90', '95', '100'] } },
+
+
+
+			{ block: 'pt-form', mods: { border: 'all',
+										shadow: 'cloud',
+										view: ['default', 'ghost', 'inverse'] } },
+			{ block: 'pt-form', elem: 'event' },
+			{ block: 'pt-form', elem: 'item', elemMods: { border: ['all', 'bottom', 'top'],
+														  distribute: ['between', 'default'],
+														 'indent-a': ['l', 'm', 's', 'xl', 'xs', 'xxl', 'xxxl'],
+														 'indent-b': ['l', 'm', 's', 'xl', 'xs', 'xxl', 'xxxl'],
+														 'indent-h': ['l', 'm', 's', 'xl', 'xs', 'xxl', 'xxxl'],
+														 'indent-l': ['l', 'm', 's', 'xl', 'xs', 'xxl', 'xxxl'],
+														 'indent-r': ['l', 'm', 's', 'xl', 'xs', 'xxl', 'xxxl'],
+														 'indent-t': ['l', 'm', 's', 'xl', 'xs', 'xxl', 'xxxl'],
+														 'indent-v': ['l', 'm', 's', 'xl', 'xs', 'xxl', 'xxxl'],
+														 'space-a': ['l', 'm', 's', 'xl', 'xs', 'xxl', 'xxxl'],
+														 'space-b': ['l', 'm', 's', 'xl', 'xs', 'xxl', 'xxxl'],
+														 'space-h': ['l', 'm', 's', 'xl', 'xs', 'xxl', 'xxxl'],
+														 'space-l': ['l', 'm', 's', 'xl', 'xs', 'xxl', 'xxxl'],
+														 'space-r': ['l', 'm', 's', 'xl', 'xs', 'xxl', 'xxxl'],
+														 'space-t': ['l', 'm', 's', 'xl', 'xs', 'xxl', 'xxxl'],
+														 'space-v': ['l', 'm', 's', 'xl', 'xs', 'xxl', 'xxxl'],
+														 'type': 'action' } },
+			{ block: 'pt-form', elem: 'label', elemMods: { width: ['default', 'inverse'] } },
+
+
+
+			{ block: 'pt-icon-plus', mods: { distribute: ['between', 'default'],
+											 indent: ['l', 'm', 's', 'xs', 'xxs'] } },
+			{ block: 'pt-icon-plus', elem: 'block' },
+			{ block: 'pt-icon-plus', elem: 'icon', elemMods: { 'indent-right': ['l', 'm', 's', 'xs', 'xxs'],
+															   'indent-left': ['l', 'm', 's', 'xs', 'xxs'],
+															   'vertical-align': ['center', 'top'] } },
+
+
+
+			{ block: 'pt-informer', mods: { border: 'all', view: ['alert', 'normal', 'success', 'warning'] } },
+			{ block: 'pt-informer', elem: 'action', elemMods: { 'space-a': ['l', 'm', 's', 'xl', 'xxl'],
+																'space-b': ['l', 'm', 's', 'xl', 'xxl'],
+																'space-h': ['l', 'm', 's', 'xl', 'xxl'],
+																'space-v': ['l', 'm', 's', 'xl', 'xxl'] } },
+			{ block: 'pt-informer', elem: 'col' },
+			{ block: 'pt-informer', elem: 'content', elemMods: { align: 'center',
+																 distribute: ['between', 'default'],
+																'space-a': ['l', 'm', 's', 'xl', 'xxl'],
+																'space-h': ['l', 'm', 's', 'xl', 'xxl'],
+																'space-v': ['l', 'm', 's', 'xl', 'xxl'] } },
+
+
+
+			{ block: 'pt-list', mods: { border: 'all', shadow: 'cloud', view: ['default', 'ghost', 'inverse'] } },
+			{ block: 'pt-list', elem: 'item', elemMods: { border: ['bottom', 'left', 'right', 'top'],
+														  distribute: ['between', 'default'],
+														 'indent-a': ['l', 'm', 's', 'xl', 'xs', 'xxl', 'xxxl'],
+														 'indent-b': ['l', 'm', 's', 'xl', 'xs', 'xxl', 'xxxl'],
+														 'indent-h': ['l', 'm', 's', 'xl', 'xs', 'xxl', 'xxxl'],
+														 'indent-l': ['l', 'm', 's', 'xl', 'xs', 'xxl', 'xxxl'],
+														 'indent-t': ['l', 'm', 's', 'xl', 'xs', 'xxl', 'xxxl'],
+														 'indent-r': ['l', 'm', 's', 'xl', 'xs', 'xxl', 'xxxl'],
+														 'indent-v': ['l', 'm', 's', 'xl', 'xs', 'xxl', 'xxxl'],
+														 'space-a': ['l', 'm', 's', 'xl', 'xs', 'xxl', 'xxs'],
+														 'space-b': ['l', 'm', 's', 'xl', 'xs', 'xxl', 'xxs'],
+														 'space-h': ['l', 'm', 's', 'xl', 'xs', 'xxl', 'xxs'],
+														 'space-l': ['l', 'm', 's', 'xl', 'xs', 'xxl', 'xxs'],
+														 'space-r': ['l', 'm', 's', 'xl', 'xs', 'xxl', 'xxs'],
+														 'space-t': ['l', 'm', 's', 'xl', 'xs', 'xxl', 'xxs'],
+														 'space-v': ['l', 'm', 's', 'xl', 'xs', 'xxl', 'xxs'] } },
+
+
+
+			{ block: 'pt-surface' },
+			{ block: 'pt-surface', elem: 'close', elemMods: { view: ['desktop', 'mobile'] } },
+			{ block: 'pt-surface', elem: 'content', elemMods: { size: ['full', 'm', 's', 'xs'],
+															   'space-a': ['l', 'm', 's', 'xl'],
+															   'space-b': ['l', 'm', 's', 'xl'],
+															   'space-h': ['l', 'm', 's', 'xl'],
+															   'space-l': ['l', 'm', 's', 'xl'],
+															   'space-r': ['l', 'm', 's', 'xl'],
+															   'space-t': ['l', 'm', 's', 'xl'],
+															   'space-v': ['l', 'm', 's', 'xl'],
+																view: ['default', 'inverse'] } },
+			{ block: 'pt-surface', elem: 'window' },
+			{ block: 'pt-surface', elem: 'window', elemMods: { view: ['tone', 'soft'] } },
+
+
+
+			{ block: 'pt-table', elemMods: { border: 'all',
+											'space-a': ['l', 'm', 's', 'xs'],
+											'space-h': ['l', 'm', 's', 'xs'],
+											'space-v': ['l', 'm', 's', 'xs'],
+											stripe: ['even', 'odd'],
+											view: ['default', 'ghost', 'inverse'] } },
+			{ block: 'pt-table', elem: 'col', elemMods: { align: 'right',
+														  width: ['5', '10', '15', '20', '25', '30', '35', '40', '45', '50', '55', '60', '65', '70', '75', '80', '85', '90', '95', '100'] } },
+			{ block: 'pt-table', elem: 'row', elemMods: { border: ['bottom', 'top'],
+														  status: ['default', 'error', 'success', 'warning'],
+														  view: 'head' } },
+
+
+
+
+			{ block: 'text', mods: { align: ['center', 'left', 'right'],
+									 decoration: 'underline',
+									 display: ['block', 'inline', 'inline-block'],
+									 indent: ['l', 'm', 's', 'xl', 'xs', 'xxl', 'xxs', 'xxxl', 'xxxs', 'xxxxl'],
+									 overflow: ['one-line', 'two-line'],
+									 size: ['l', 'm', 's', 'xl', 'xs', 'xxl', 'xxxl', 'xxxxl'],
+									 spacing: ['m', 's', 'xs'],
+									 style: 'italic',
+									 transform: 'uppercase',
+									 type: ['blockquote', 'h1', 'h2', 'h3', 'p'],
+									 view: ['alert', 'brand', 'disable', 'ghost', 'income', 'inverse', 'letter', 'link-minor', 'link', 'personal', 'promo', 'secondary', 'success'],
+									 weight: ['bold', 'light', 'regular', 'semibold', 'thin'] } },
+
+
+
+			{ block: 'icon', mods: { 'add-3': ['m-action', 'm-inverse', 'm-link', 'm-primary', 's-action', 's-disable', 's-ghost', 's-inverse', 's-link', 's-primary', 's-secondary'],
+									 'allert-1': ['m-action', 'm-inverse', 'm-link', 'm-primary', 's-action', 's-disable', 's-ghost', 's-inverse', 's-link', 's-primary', 's-secondary'],
+									 'allert-2': ['m-action', 'm-inverse', 'm-link', 'm-primary', 's-action', 's-disable', 's-ghost', 's-inverse', 's-link', 's-primary', 's-secondary'],
+									 'arrow-down': ['m-action', 'm-inverse', 'm-link', 'm-primary', 's-action', 's-disable', 's-ghost', 's-inverse', 's-link', 's-primary', 's-secondary'],
+									 'arrow-down-2': ['m-action', 'm-inverse', 'm-link', 'm-primary', 's-action', 's-disable', 's-ghost', 's-inverse', 's-link', 's-primary', 's-secondary'],
+									 'arrow-left': ['m-action', 'm-inverse', 'm-link', 'm-primary', 's-action', 's-disable', 's-ghost', 's-inverse', 's-link', 's-primary', 's-secondary'],
+									 'arrow-right': ['m-action', 'm-inverse', 'm-link', 'm-primary', 's-action', 's-disable', 's-ghost', 's-inverse', 's-link', 's-primary', 's-secondary'],
+									 'arrow-up': ['m-action', 'm-inverse', 'm-link', 'm-primary', 's-action', 's-disable', 's-ghost', 's-inverse', 's-link', 's-primary', 's-secondary'],
+									 'arrow-up-2': ['m-action', 'm-inverse', 'm-link', 'm-primary', 's-action', 's-disable', 's-ghost', 's-inverse', 's-link', 's-primary', 's-secondary'],
+									 'back': ['m-action', 'm-inverse', 'm-link', 'm-primary', 's-action', 's-disable', 's-ghost', 's-inverse', 's-link', 's-primary', 's-secondary'],
+									 'check': ['m-action', 'm-inverse', 'm-link', 'm-primary', 's-action', 's-disable', 's-ghost', 's-inverse', 's-link', 's-primary', 's-secondary'],
+									 'close': ['m-action', 'm-inverse', 'm-link', 'm-primary', 's-action', 's-disable', 's-ghost', 's-inverse', 's-link', 's-primary', 's-secondary'],
+									 'comment': ['m-action', 'm-inverse', 'm-link', 'm-primary', 's-action', 's-disable', 's-ghost', 's-inverse', 's-link', 's-primary', 's-secondary'],
+									 'crop': ['m-action', 'm-inverse', 'm-link', 'm-primary', 's-action', 's-disable', 's-ghost', 's-inverse', 's-link', 's-primary', 's-secondary'],
+									 'export': ['m-action', 'm-inverse', 'm-link', 'm-primary', 's-action', 's-disable', 's-ghost', 's-inverse', 's-link', 's-primary', 's-secondary'],
+									 'fave-1': ['m-action', 'm-inverse', 'm-link', 'm-primary', 's-action', 's-disable', 's-ghost', 's-inverse', 's-link', 's-primary', 's-secondary'],
+									 'front': ['m-action', 'm-inverse', 'm-link', 'm-primary', 's-action', 's-disable', 's-ghost', 's-inverse', 's-link', 's-primary', 's-secondary'],
+									 'globe': ['m-action', 'm-inverse', 'm-link', 'm-primary', 's-action', 's-disable', 's-ghost', 's-inverse', 's-link', 's-primary', 's-secondary'],
+									 'hamburger': ['m-action', 'm-inverse', 'm-link', 'm-primary', 's-action', 's-disable', 's-ghost', 's-inverse', 's-link', 's-primary', 's-secondary'],
+									 'lock': ['m-action', 'm-inverse', 'm-link', 'm-primary', 's-action', 's-disable', 's-ghost', 's-inverse', 's-link', 's-primary', 's-secondary'],
+									 'mail-1': ['m-action', 'm-inverse', 'm-link', 'm-primary', 's-action', 's-disable', 's-ghost', 's-inverse', 's-link', 's-primary', 's-secondary'],
+									 'phone-1': ['m-action', 'm-inverse', 'm-link', 'm-primary', 's-action', 's-disable', 's-ghost', 's-inverse', 's-link', 's-primary', 's-secondary'],
+									 'photo': ['m-action', 'm-inverse', 'm-link', 'm-primary', 's-action', 's-disable', 's-ghost', 's-inverse', 's-link', 's-primary', 's-secondary'],
+									 'picture': ['m-action', 'm-inverse', 'm-link', 'm-primary', 's-action', 's-disable', 's-ghost', 's-inverse', 's-link', 's-primary', 's-secondary'],
+									 'qua': ['m-action', 'm-inverse', 'm-link', 'm-primary', 's-action', 's-disable', 's-ghost', 's-inverse', 's-link', 's-primary', 's-secondary'],
+									 'refresh': ['m-action', 'm-inverse', 'm-link', 'm-primary', 's-action', 's-disable', 's-ghost', 's-inverse', 's-link', 's-primary', 's-secondary'],
+									 'repeat': ['m-action', 'm-inverse', 'm-link', 'm-primary', 's-action', 's-disable', 's-ghost', 's-inverse', 's-link', 's-primary', 's-secondary'],
+									 'save': ['m-action', 'm-inverse', 'm-link', 'm-primary', 's-action', 's-disable', 's-ghost', 's-inverse', 's-link', 's-primary', 's-secondary'],
+									 'search': ['m-action', 'm-inverse', 'm-link', 'm-primary', 's-action', 's-disable', 's-ghost', 's-inverse', 's-link', 's-primary', 's-secondary'],
+									 'statistic': ['m-action', 'm-inverse', 'm-link', 'm-primary', 's-action', 's-disable', 's-ghost', 's-inverse', 's-link', 's-primary', 's-secondary'],
+									 'time': ['m-action', 'm-inverse', 'm-link', 'm-primary', 's-action', 's-disable', 's-ghost', 's-inverse', 's-link', 's-primary', 's-secondary'],
+									 'trash': ['m-action', 'm-inverse', 'm-link', 'm-primary', 's-action', 's-disable', 's-ghost', 's-inverse', 's-link', 's-primary', 's-secondary'],
+									 'user': ['m-action', 'm-inverse', 'm-link', 'm-primary', 's-action', 's-disable', 's-ghost', 's-inverse', 's-link', 's-primary', 's-secondary'],
+									 'wait-2': ['m-action', 'm-inverse', 'm-link', 'm-primary', 's-action', 's-disable', 's-ghost', 's-inverse', 's-link', 's-primary', 's-secondary'] } }
+		]
+	}
+])
---
+++ desktop.bundles/bem-design/bem-design.bemjson.js
@@ -0,0 +1,20 @@
+module.exports = {
+	block: 'page',
+	title: 'BEM DESIGN',
+	favicon: '/favicon.ico',
+	lang: 'ru',
+	head: [
+		{ elem: 'meta', attrs: { name: 'description', content: '' } },
+		{ elem: 'meta', attrs: { name: 'viewport', content: 'width=device-width, initial-scale=1' } },
+		{ elem: 'css', url: 'bem-design.min.css' },
+	],
+	scripts: [{ elem: 'js', url: 'bem-design.min.js' }],
+	mix: {
+		block: 'theme',
+		mods: { color: 'default', space: 'default', size: 'default' }
+	},
+	content: [
+	{
+		block: 'cdn'
+	}]
+};
